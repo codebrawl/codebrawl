2862ffec31786129ae7dcbc5f1e4ed048bf1f675
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2011-05-22 23:40:37 +0200

    Vanilla Rails Edge

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'http://rubygems.org'
+
+gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'sqlite3'
+
+# Asset template engines
+gem 'json'
+gem 'sass'
+gem 'coffee-script'
+gem 'uglifier'
+
+gem 'jquery-rails'
+
+# Use unicorn as the web server
+# gem 'unicorn'
+
+# Deploy with Capistrano
+# gem 'capistrano'
+
+# To use debugger
+# gem 'ruby-debug19', :require => 'ruby-debug'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codebrawl::Application.config.secret_token = 'be8f1d5dcfa0e0170604c840142dd3c831b8937bb09f999b15a8b45aab1d18a44387a27694987ed3460d074c45ed6cfb2ba3500e903e4f16c30a45b36201e9b3'
