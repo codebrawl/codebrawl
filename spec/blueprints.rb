d65e9defaf5617d49c848c45d1ee37f2ac26a98b
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2011-05-23 12:34:28 +0200

    Remove spec/blueprints.rb

--- spec/blueprints.rb
+++
@@ -1,7 +0,0 @@
-require 'machinist/mongoid'
-require 'sham'
-require 'faker'
-
-Contest.blueprint do
-  name { Faker::Lorem.words }
-end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,8 +6,6 @@ Spork.prefork do
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
 
-  require 'blueprints'
-
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
