49362d38bb7f1fff3a839e0f1f3dbe8813d46614
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2011-05-23 19:47:44 +0200

    Add Contest seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require "#{Rails.root}/spec/support/blueprints"
+Contest.delete_all
+
+3.times { Contest.make }
--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -2,7 +2,7 @@ require 'machinist/mongoid'
 require 'faker'
 
 Contest.blueprint do
-  name { Faker::Lorem.sentence }
+  name { Faker::Lorem.words.join(' ').capitalize }
   description { Faker::Lorem.sentence(25) }
   starting_on { Date.parse('May 23 2011') }
 end
