151d620cd275b0f0aa8be5c035c61f3a29270b60
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2011-05-23 12:42:31 +0200

    Add Contest#state and set it to 'open' by default

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -3,6 +3,7 @@ class Contest
 
   field :name, :type => String
   field :starts_at, :type => DateTime
+  field :state, :type => String, :default => 'open'
 
   validates :name, :starts_at, :presence => true
 end
--- spec/models/contest_spec.rb
+++ spec/models/contest_spec.rb
@@ -12,12 +12,24 @@ describe 'Contest' do
 
   context '.save!' do
 
-    it 'should have name' do
-      Contest.create.errors[:name].should include 'can\'t be blank'
+    context 'when keeping all fields empty' do
+
+      it 'should have name' do
+        Contest.create.errors[:name].should include 'can\'t be blank'
+      end
+
+      it 'should have a start date' do
+        Contest.create.errors[:starts_at].should include 'can\'t be blank'
+      end
+
     end
 
-    it 'should have a start date' do
-      Contest.create.errors[:starts_at].should include 'can\'t be blank'
+    context 'when creating a valid contest' do
+
+      it 'sets the state to open by default' do
+        Contest.make.state.should == 'open'
+      end
+
     end
 
   end
