4762b67e75e366593901bbc66d9d664e4a7a661b
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2011-05-29 12:47:21 +0200

    Clean up the login spec and move the login logic

--- spec/acceptance/acceptance_helper.rb
+++ spec/acceptance/acceptance_helper.rb
@@ -1,2 +1,16 @@
 require 'spec_helper'
 require 'capybara/rspec'
+
+Capybara.default_host = 'example.org'
+OmniAuth.config.test_mode = true
+
+def login_via_github
+  OmniAuth.config.add_mock(:github, {
+      :uid => '12345',
+      :user_info => {
+        :nickname => 'alice'
+      }
+  })
+
+  visit '/auth/github/'
+end
