8e6dd08902861be55fe38b46e878ad26095a358a
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2011-06-02 00:24:35 +0200

    Create entries for the Contests in the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,9 @@
 require "#{Rails.root}/spec/support/blueprints"
 Contest.delete_all
 
-3.times { |i| Contest.make(:starting_on => i.weeks.ago.to_time) }
+3.times do |i|
+  Contest.make(
+    :starting_on => i.weeks.ago.to_time,
+    :entries => [Entry.make] * 3
+  )
+end
