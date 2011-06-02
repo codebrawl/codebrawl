3f2899ea6833b9df6e7623522e6a7da6b3379b31
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2011-06-02 17:31:53 +0200

    Link back to the homepage from the site header

--- spec/acceptance/homepage_spec.rb
+++ spec/acceptance/homepage_spec.rb
@@ -14,6 +14,14 @@ feature 'Homepage' do
     scenario 'see the Codebrawl header' do
       page.should have_content 'Codebrawl'
     end
+    
+    scenario 'return to the homepage after clicking the header' do
+      click_link 'Fun with ChunkyPNG'
+      click_link 'Codebrawl'
+      ['Euler #74', 'Fun with ChunkyPNG'].each do |name|
+        page.should have_link name
+      end
+    end
 
     scenario 'see a list of contest names' do
       ['Euler #74', 'Fun with ChunkyPNG'].each do |name|
