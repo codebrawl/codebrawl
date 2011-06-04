7dd7bfe5ad9fb1e649afcbda55f4563f1d6956a7
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2011-06-05 01:27:05 +0200

    Ignore empty voting forms

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,8 +2,11 @@ class VotesController < ApplicationController
 
   def create
     @contest = Contest.find_by_slug(params[:contest_id])
-    params[:votes].each do |key, value|
-      @contest.entries.find(key).votes.create!(value.merge({:user_id => current_user.id}))
+
+    unless params[:votes].blank?
+      params[:votes].each do |key, value|
+        @contest.entries.find(key).votes.create!(value.merge({:user_id => current_user.id}))
+      end
     end
 
     redirect_to @contest
--- spec/acceptance/voting_spec.rb
+++ spec/acceptance/voting_spec.rb
@@ -2,25 +2,32 @@ require 'acceptance/acceptance_helper'
 
 feature 'Voting' do
 
-  scenario 'successfully vote for an entry' do
+  background do
     login_via_github
 
     # TODO: stub `Contest#state` instead setting the voting date
-
-    contest = Contest.make(
+    @contest = Contest.make(
+      :name => 'Rspec extensions',
       :entries => [Entry.make],
       :voting_on => Date.yesterday.to_time
     )
 
-    visit "contests/#{contest.slug}"
+    visit "contests/#{@contest.slug}"
+  end
 
-    within "#entry_#{contest.entries.first.id}" do
+  scenario 'vote without selecting any scores' do
+    click_button 'Submit your votes'
+    page.should have_content 'Rspec extensions'
+  end
+
+  scenario 'successfully vote for an entry' do
+    within "#entry_#{@contest.entries.first.id}" do
       choose '4'
     end
 
     click_button 'Submit your votes'
 
-    within "#entry_#{contest.entries.first.id}" do
+    within "#entry_#{@contest.entries.first.id}" do
       page.should have_content 'You voted 4/5'
     end
 
