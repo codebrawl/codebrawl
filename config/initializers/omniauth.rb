b054b895d0603a06c594f71f351f7de1ff5bc68b
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2011-06-15 09:52:29 +0200

    Authorize with Gist access

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github,
     'e17efcf41c75f7aef25b',
     'fd74b7c6d05f546c162ef6c29e7f055515a50d53',
-    {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
+    {:scope => 'gist', :client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
 end
