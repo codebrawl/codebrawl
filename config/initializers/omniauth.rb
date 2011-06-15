7fc39a50ccc357d752fca6399798f3dd7b3cbd12
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2011-06-15 21:58:43 +0200

    Add the production Github credentials

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github,
-    'e17efcf41c75f7aef25b',
-    'fd74b7c6d05f546c162ef6c29e7f055515a50d53',
+    Rails.env == 'production' ? 'a7e4f66ae5576162b1ed': 'e17efcf41c75f7aef25b',
+    Rails.env == 'production' ? '407769aad0c9ddf74e530851fa9ee040e62029e2': 'fd74b7c6d05f546c162ef6c29e7f055515a50d53',
     {:scope => 'gist', :client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
 end
