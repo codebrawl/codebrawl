dc8b98fe23a12c6b46e9e2e9d0411974aaafe0e9
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2011-06-23 09:21:03 +0200

    Make the seeds trainproof

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,22 @@
 require 'fabrication'
 require 'faker'
+require 'vcr'
+
+VCR.config do |c|
+  c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  c.stub_with :fakeweb
+end
 
 Dir[Rails.root.join("spec/fabricators/**/*.rb")].each {|f| require f}
 [Contest, User].each { |model| model.delete_all }
 
-3.times do |i|
-  Fabricate(
-    :contest,
-    :starting_on => (i.weeks + 1.day).ago.to_time,
-    :entries => [
-      Fabricate(:entry, :gist_id => '830060'),
-      Fabricate(:entry, :gist_id => '813725'),
-      Fabricate(:entry, :gist_id => '676219')
-    ]
-  )
+VCR.use_cassette('existing_gist') do
+  3.times do |i|
+    contest = Fabricate(
+      :contest,
+      :starting_on => (i.weeks + 1.day).ago.to_time,
+    )
+
+    3.times { Fabricate(:entry_with_files, :contest => contest) }
+  end
 end
