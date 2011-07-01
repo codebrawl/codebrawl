8ef569d148d0b48ed62b0dbabf4dfc2e244449dc
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2011-07-01 18:53:15 +0200

    Remove #who, need to find a better way to display it

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,22 +29,6 @@ function get_gist_comments(gist_id, element){
 
 $(document).ready(function(){
 
-  $('#menu').hover(function() {
-    var fade = $('span#who');
-    if (fade.is(':animated')) {
-      fade.stop().fadeTo(200, 1);
-    } else {
-      fade.fadeIn(200);
-    }
-  }, function () {
-    var fade = $('span#who');
-    if (fade.is(':animated')) {
-      fade.stop().fadeTo(200, 0);
-    } else {
-      fade.fadeOut(200);
-    }
-  })
-
   $('.comments').each(function(){
     get_gist_comments($(this).data('gist_id'), $(this));
   });
