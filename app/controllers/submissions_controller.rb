7f15302ab30d773737d25fe06986bec02b2d13d3
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2011-07-05 09:07:04 +0200

    Switch back to Wufoo and add a spec for when Wufoo fails

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -17,7 +17,7 @@ class SubmissionsController < ApplicationController
     if response.success?
       redirect_to root_path, :notice => 'Thanks for your submission! We\'ll check it out and let you know if we decide to use it.'
     else
-      redirect_to root_path, :alert => 'Something went wrong while sending your suggestion. Please try again later.'
+      redirect_to root_path, :alert => 'Something went wrong while sending your submission. Please try again later.'
     end
   end
 end
