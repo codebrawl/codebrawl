11ad43570822610811a0a6cc2e87cd45e3eadd6f
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2011-07-06 20:15:32 +0200

    Add User#calculate_points and User#calculate_points!

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,23 @@ class User
   field 'name', :type => String
   field 'github_id', :type => Integer
   field 'participations', :type => Array, :default => []
+  field 'points', :type => Integer
 
   validates :login, :presence => true
 
   has_gravatar
 
   alias_method :to_param, :login
+
+  def calculate_points
+    participations.inspect
+    participations.map do |participation|
+      participation['points']
+    end.inject { |a,b| a + b }
+  end
+
+  def calculate_points!
+    update_attribute(:points, calculate_points)
+  end
+
 end
