50b3ecb11e9feb672b49f994fc73fbc820ae5feb
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2011-07-14 09:11:25 +0200

    Show the comments and allow users to comment on the finished contest page

--- spec/acceptance/comments_spec.rb
+++ spec/acceptance/comments_spec.rb
@@ -1,5 +1,13 @@
 require 'acceptance/acceptance_helper'
 
+shared_examples_for 'an entry with a voting box' do
+  scenario 'add a comment from the entry list' do
+    Gist.expects(:comment).with('866948', 't0k3n', 'Another comment!')
+    fill_in 'Comment', :with => 'Another comment!'
+    click_button 'Comment'
+  end
+end
+
 feature 'Comments' do
 
   context 'when on a contest page which is in the voting period' do
@@ -33,15 +41,27 @@ feature 'Comments' do
         click_button 'Vote'
       end
       
-      scenario 'add a comment from the entry list' do
-        Gist.expects(:comment).with('866948', 't0k3n', 'Another comment!')
-        fill_in 'Comment', :with => 'Another comment!'
-        click_button 'Comment'
-      end
+      it_should_behave_like 'an entry with a voting box'
+
+    end
+
+  end
+  
+  context 'when on a finished contest page' do
+    
+    background do
+      # TODO: stub `Contest#state` instead setting the voting date
+      @contest = Fabricate(
+        :contest,
+        :entries => [Fabricate.build(:entry_with_files)],
+        :closing_on => Date.yesterday.to_time
+      )
       
+      visit "contests/#{@contest.slug}"
+      login_via_github
     end
     
-    
+    it_should_behave_like 'an entry with a voting box'
     
   end
   
