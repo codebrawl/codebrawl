c2167af12abc69fb96da15661440841030b8d45a
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2011-07-23 11:27:12 +0200

    Show the entry comments in the entry viewer

--- app/assets/javascripts/entry_viewer.js
+++ app/assets/javascripts/entry_viewer.js
@@ -1,24 +1,45 @@
 //= require jquery
 //= require address
+//= require showdown
 
 $.address.change(function(event) {
   filename = event.value.substring(1)
   if(filename.length > 0){
     $('#sidebar li').each(function(){ $(this).removeClass('current')})
-    $('ul#files li').hide()
+    $('ul#files li[data-filename]').hide()
+    console.log('ul#drawer li[data-filename="' + filename + '"]')
     $('ul#drawer li[data-filename="' + filename + '"]').addClass('current');
     $('ul#files li[data-filename="' + filename + '"]').show().addClass('current');
   }
 });
 
 $(document).ready(function(){
+  var converter = new Showdown.converter();
+  var comments = $('li[data-filename="comments"]')
+  var gist_id = comments.data('gist_id')
+
+  $.getJSON(
+    'https://gist.github.com/api/v1/json/' + gist_id + '?callback=?', function(response){
+
+      $('a[href="#comments"]').prepend('<span>' + response['gists'][0]['comments'].length + '</span> ')
+
+      $.each(response['gists'][0]['comments'], function(key, val) {
+        comments.append(
+          '<li><span class="filename"><img class="gravatar" src="http://gravatar.com/avatar/' + val.gravatar_id + '.png?r=PG&s=20"/> ' + val.user + '</span>' +
+          converter.makeHtml(val.body) +
+          '</li>'
+        );
+      });
+    }
+  );
+
   $('#sidebar').append('<ul id="drawer"/>');
   $('input[type="submit"]').hide();
   $('input').change(function(){
     $(this).parent().submit();
   })
 
-  $('ul#files li').each(function(){
+  $('ul#files li[data-filename]').each(function(){
     $('ul#drawer').append(
       '<li data-filename="' + $(this).attr('data-filename') + '">' +
       '<a href="#' + $(this).attr('data-filename') + '">' +
