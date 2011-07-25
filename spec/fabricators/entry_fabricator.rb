e6836e7940330da3550976e475a761b1392e0706
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2011-07-25 09:08:28 +0200

    Add a simple Ruby file to the Entry Fabricator

--- spec/fabricators/entry_fabricator.rb
+++ spec/fabricators/entry_fabricator.rb
@@ -6,6 +6,9 @@ end
 
 Fabricator(:entry_with_files, :from => :entry) do
   files {
-    { 'README' => { 'content' => Faker::Lorem.sentence(25) } }
+    {
+      'README' => { 'content' => Faker::Lorem.sentence(25) },
+      'implementation*rb' => { 'content' => "def foo\n  bar + baz\nend"}
+    }
   }
 end
