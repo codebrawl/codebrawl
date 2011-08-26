974670ce329433257a6184e4b0eec7810ed0041e
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2011-08-26 09:58:06 +0200

    Move User@calculate_average_points to Points

--- lib/points.rb
+++ lib/points.rb
@@ -4,4 +4,9 @@ module Points
     participations.map { |p| p['points'] }.inject(&:+) || 0
   end
 
+  def calculate_average_points
+    return 0.0 if participations.empty?
+    calculate_points.to_f / participations.length
+  end
+
 end
