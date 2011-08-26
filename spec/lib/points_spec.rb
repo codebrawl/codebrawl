e6f8afbe01c49de4cefe6402273bda83b60ea773
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2011-08-26 09:58:06 +0200

    Move User@calculate_average_points to Points

--- spec/lib/points_spec.rb
+++ spec/lib/points_spec.rb
@@ -7,13 +7,13 @@ end
 
 describe ObjectWithPoints do
 
-  describe '#calculate_points' do
+  let(:object) do
+    object = ObjectWithPoints.new
+    object.stubs(:participations).returns([])
+    object
+  end
 
-    let(:object) do
-      object = ObjectWithPoints.new
-      object.stubs(:participations).returns([])
-      object
-    end
+  describe '#calculate_points' do
 
     subject { object.calculate_points }
 
@@ -32,6 +32,26 @@ describe ObjectWithPoints do
       it 'should calculate the total points' do
         should == 60
       end
+
+    end
+
+  end
+
+  describe '#calculate_average_points' do
+
+    subject { object.calculate_average_points }
+
+    it { should == 0.0 }
+
+    context 'when having participations with scores' do
+
+      subject do
+        object.stubs(:calculate_points).returns(80)
+        object.stubs(:participations).returns([{},{},{}])
+        object.calculate_average_points
+      end
+
+      it { should == 26.666666666666668 }
 
     end
 
