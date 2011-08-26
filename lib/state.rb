9230e856d5f2b587486d257cdaebf0147b4943ca
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2011-08-26 21:40:23 +0200

    Move the state methods from Contest to State

---
+++ lib/state.rb
@@ -0,0 +1,27 @@
+require 'active_support/string_inquirer'
+require 'active_support/core_ext/module/delegation'
+
+module State
+
+  def state
+    case
+    when Time.now.utc >= closing_at then 'finished'
+    when Time.now.utc >= voting_at then 'voting'
+    when Time.now.utc >= starting_at then 'open'
+    else
+      'pending'
+    end
+  end
+
+  def inquirable_state
+    ActiveSupport::StringInquirer.new(state)
+  end
+
+  delegate \
+    :pending?,
+    :open?,
+    :voting?,
+    :finished?,
+    :to => :inquirable_state
+
+end
