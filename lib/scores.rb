2598835f1eca1aed84af62be0e00858092bb0304
Vasiliy Ermolovich <younash@gmail.com>
2011-08-30 23:21:49 +0300

    refactor injects/maps

--- lib/scores.rb
+++ lib/scores.rb
@@ -2,7 +2,7 @@ module Scores
 
   def calculate_average_score
     return 0.0 if participations.empty?
-    participations.map { |p| p['score'] }.inject(:+) / participations.length
+    participations.inject(0) { |sum, p| sum + p['score'] } / participations.length
   end
 
 
