faa97f019b599ab242199e03a9842c77e741bed5
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2011-09-01 07:51:55 +0200

    Hide the suggestion list when it's empty

--- spec/acceptance/suggestions_spec.rb
+++ spec/acceptance/suggestions_spec.rb
@@ -3,105 +3,116 @@ require 'acceptance/acceptance_helper'
 feature 'Suggestions' do
 
   context 'on the index page' do
-
-    background(:all) do
-      Fabricate(
-        :suggestion,
-        :name => 'Terminal admin',
-        :votes => [{:score => -1}] * 2
-      )
-
-      Fabricate(
-        :suggestion,
-        :name => 'RSpec formatters',
-        :votes => [{:score => 1}] * 4
-      )
-
-      Fabricate(
-        :suggestion,
-        :name => 'Whyday'
-      )
-
+    background do
       visit 'suggestions'
     end
 
-    scenario 'see the list of suggestions, ordered by score' do
-
-      within(:xpath, '//tbody/tr[1]') do
-        page.should have_content 'RSpec formatters'
-      end
-
-      within(:xpath, '//tbody/tr[2]') do
-        page.should have_content 'Whyday'
-      end
-
-      within(:xpath, '//tbody/tr[3]') do
-        page.should have_content 'Terminal admin'
-      end
-
+    scenario 'do not see the suggestion list' do
+      page.should have_no_content 'Idea'
+      page.should have_no_content 'Score'
     end
 
-    scenario 'see the suggestion scores' do
+    context 'when having some suggestions' do
 
-      within(:xpath, '//tbody/tr[1]') do
-        page.should have_content '4'
+      background(:all) do
+        Fabricate(
+          :suggestion,
+          :name => 'Terminal admin',
+          :votes => [{:score => -1}] * 2
+        )
+
+        Fabricate(
+          :suggestion,
+          :name => 'RSpec formatters',
+          :votes => [{:score => 1}] * 4
+        )
+
+        Fabricate(
+          :suggestion,
+          :name => 'Whyday'
+        )
+
       end
 
-      within(:xpath, '//tbody/tr[2]') do
-        page.should have_content '0'
-      end
-
-      within(:xpath, '//tbody/tr[3]') do
-        page.should have_content '-2'
-      end
-
-    end
-
-    scenario 'do not see the voting controls' do
-      page.should have_no_css('input.upvote')
-      page.should have_no_css('input.downvote')
-    end
-
-    context 'after logging in' do
-
-      background do
-        login_via_github
-        visit '/suggestions'
-      end
-
-      scenario 'upvote a suggestion' do
-
-        find('input.upvote').click
-
-        page.should have_content 'Thanks for voting!'
+      scenario 'see the list of suggestions, ordered by score' do
 
         within(:xpath, '//tbody/tr[1]') do
-          page.should have_content '5'
+          page.should have_content 'RSpec formatters'
+        end
+
+        within(:xpath, '//tbody/tr[2]') do
+          page.should have_content 'Whyday'
+        end
+
+        within(:xpath, '//tbody/tr[3]') do
+          page.should have_content 'Terminal admin'
         end
 
       end
 
-      scenario 'downvote a suggestion' do
-
-        find('input.downvote').click
-
-        page.should have_content 'Thanks for voting!'
+      scenario 'see the suggestion scores' do
 
         within(:xpath, '//tbody/tr[1]') do
           page.should have_content '4'
         end
 
+        within(:xpath, '//tbody/tr[2]') do
+          page.should have_content '0'
+        end
+
+        within(:xpath, '//tbody/tr[3]') do
+          page.should have_content '-2'
+        end
+
       end
 
-      scenario 'create a suggestion' do
+      scenario 'do not see the voting controls' do
+        page.should have_no_css('input.upvote')
+        page.should have_no_css('input.downvote')
+      end
 
-        fill_in :name, :with => 'Whyday'
-        fill_in :description, :with => "Friday is Whyday, so let's have a Whyday contest!"
-        click_button 'Suggest'
+      context 'after logging in' do
 
-        page.should have_content 'Thanks for your suggestion!'
-        page.should have_content 'Whyday'
-        page.should have_content "Friday is Whyday, so let's have a Whyday contest!"
+        background do
+          login_via_github
+          visit '/suggestions'
+        end
+
+        scenario 'upvote a suggestion' do
+
+          find('input.upvote').click
+
+          page.should have_content 'Thanks for voting!'
+
+          within(:xpath, '//tbody/tr[1]') do
+            page.should have_content '5'
+          end
+
+        end
+
+        scenario 'downvote a suggestion' do
+
+          find('input.downvote').click
+
+          page.should have_content 'Thanks for voting!'
+
+          within(:xpath, '//tbody/tr[1]') do
+            page.should have_content '4'
+          end
+
+        end
+
+        scenario 'create a suggestion' do
+
+          fill_in :name, :with => 'Whyday'
+          fill_in :description, :with => "Friday is Whyday, so let's have a Whyday contest!"
+          click_button 'Suggest'
+
+          page.should have_content 'Thanks for your suggestion!'
+          page.should have_content 'Whyday'
+          page.should have_content "Friday is Whyday, so let's have a Whyday contest!"
+
+        end
 
       end
 
