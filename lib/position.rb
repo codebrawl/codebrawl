8c585b181d8d851b8582f04edfb98d285e4a7ad6
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2011-09-09 19:10:47 +0200

    Move the average position and score calculations to ArrayAverage

--- lib/position.rb
+++ lib/position.rb
@@ -1,9 +1,9 @@
+require File.expand_path('lib/array_average')
+
 module Position
 
   def calculate_average_position
-    participations_with_positions = participations.select { |p| p['position'] }
-    return 0.0 if participations_with_positions.empty?
-    participations_with_positions.inject(0) { |sum, p| sum + p['position'] } / participations_with_positions.length.to_f
+    participations.map { |p| p['position'] }.average
   end
 
 end
